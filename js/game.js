4c42ba16da53124e8137c477cb8e71579412e38e
lach1an <87818532+lach1an@users.noreply.github.com>
2021-11-01 18:58:53 +0000

    Update game.js
    bee movement added

--- js/game.js
+++ js/game.js
@@ -206,3 +206,26 @@ function makeBees() {
 }
 
 
+function moveBees() {
+   //get speed input field value
+   let speed = $("#speedBees").val();
+   
+  //move each bee to a random location
+   for (let i = 0; i < bees.length; i++) {
+     let dx = getRandomInt(2 * speed) - speed;
+     let dy = getRandomInt(2 * speed) - speed;
+     bees[i].move(dx, dy);
+   }
+}
+
+  function updateBees() { // update loop for game
+     //move the bees randomly
+     moveBees();
+    
+     //use a fixed update period
+     let period = $("#periodTimer").val();
+     //update the timer for the next move
+     updateTimer = setTimeout('updateBees()', period);
+}
+
+
