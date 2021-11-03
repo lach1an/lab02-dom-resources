6cf1510b3b7925d127af57db34e354214546a90f
lach1an <87818532+lach1an@users.noreply.github.com>
2021-11-03 14:04:38 +0000

    Update game.js

--- js/game.js
+++ js/game.js
@@ -115,7 +115,7 @@ function Bear() {
        //use a fixed update period
        let period = $("#periodTimer").val();
        //update the timer for the next move
-       updateTimer = setTimeout(updateBees(), period);
+       updateTimer = setTimeout('updateBees()', period);
      }
     
   
